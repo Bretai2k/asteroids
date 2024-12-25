5cfb0ea21ec73a98a199cf7ce883f62cd3e15de1
bretai2k <bretai2k@yahoo.com>
2024-12-25 16:29:57 -0600

    Updates to build player ship to screen

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting asteroids!")
@@ -11,13 +12,24 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    fps = pygame.time.Clock()
+    dt = 0
+
     while True:
         screen.fill(color="black", rect=None, special_flags=0)
+
+        player.draw(screen)
+
         pygame.display.flip()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        fps.tick(60)
+        dt = fps.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
