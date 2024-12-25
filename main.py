5af39f25604e9bf4809e7061815cd08c94694c5a
bretai2k <bretai2k@yahoo.com>
2024-12-25 07:06:29 -0600

    Start game, create screen, and close game

---
+++ main.py
@@ -0,0 +1,23 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+def main():
+    print("Starting asteroids!")
+    print("Screen width: 1280")
+    print("Screen height: 720")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        screen.fill(color="black", rect=None, special_flags=0)
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+if __name__ == "__main__":
+    main()
